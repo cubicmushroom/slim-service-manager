8512a459a976d40d35470556fd502c642d669566
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-02-12 18:29:45 +0000

    Added AspectMock kernel config

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,12 @@
 <?php
 // This is global bootstrap for autoloading
+
+include __DIR__.'/../vendor/autoload.php';
+
+$kernel = \AspectMock\Kernel::getInstance();
+$kernel->init(
+    [
+        'debug'        => true,
+        'includePaths' => [__DIR__ . '/../src']
+    ]
+);
